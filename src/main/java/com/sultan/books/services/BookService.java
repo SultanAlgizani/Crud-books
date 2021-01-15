af3647727829f566c9328b9e6a0a9c0b69d00135
SultanAlgizani <sultan_algizani@hotmail.com>
2021-01-15 04:29:28 +0300

    Update and Delete

---
+++ src/main/java/com/sultan/books/services/BookService.java
@@ -0,0 +1,58 @@
+package com.sultan.books.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.sultan.books.models.Book;
+import com.sultan.books.repositories.BookRepository;
+@Service
+public class BookService {
+
+    // adding the book repository as a dependency
+    private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    
+    // retrieves one book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    
+    //update a book
+    public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+        if (bookRepository.findById(id).isPresent()) {
+            Book b = bookRepository.findById(id).get();
+            b.setTitle(title);
+            b.setDescription(desc);
+            b.setLanguage(lang);
+            b.setNumberOfPages(numOfPages);
+            return bookRepository.save(b);
+        }
+        return null;
+    }
+    
+    //delete a book
+	public void deleteBook(Long id) {
+		bookRepository.deleteById(id);
+	}
+	
+}
